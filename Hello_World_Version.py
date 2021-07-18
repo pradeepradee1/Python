6d34d486bb4f5ed51e65f1513a3b14b9a6ee3390
Pradeep <pradeepradee1@gmail.com>
2021-07-18 21:20:32 +0530

    Apple and Bannana and Cat are Included

--- Hello_World_Version.py
+++ Hello_World_Version.py
@@ -1,8 +1,8 @@
 a=1
 if a < 10 :
-    print("Yes It is Apple and Bat")
+    print("Yes It is Apple and Bat and Cat")
 else:
-    print("No It isn't Apple and Bat")
+    print("No It isn't Apple and Bat and Cat")
 
 
 

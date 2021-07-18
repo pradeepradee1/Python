934c9c04ebb748b049753feae55a5c91acf2f7aa
Pradeep <pradeepradee1@gmail.com>
2021-07-18 20:16:10 +0530

    Added Orange

--- Hello_World_Version.py
+++ Hello_World_Version.py
@@ -1,7 +1,7 @@
 a=1
 if a < 10 :
-    print("Welcome_Hello_World")
-    print("bannana included")
+    print("Welcome_Hello_World") 
+    print("Changes_in_orange")
 else:
     print("Not_Welcome_Hello_World")
 

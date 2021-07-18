745bf23e02bc07f5ace745e8dc284bb8b207f223
Pradeep <pradeepradee1@gmail.com>
2021-07-18 20:22:57 +0530

    Revert "Added Orange"
    This reverts commit 934c9c04ebb748b049753feae55a5c91acf2f7aa.

--- Hello_World_Version.py
+++ Hello_World_Version.py
@@ -1,7 +1,7 @@
 a=1
 if a < 10 :
-    print("Welcome_Hello_World") 
-    print("Changes_in_orange")
+    print("Welcome_Hello_World")
+    print("bannana included")
 else:
     print("Not_Welcome_Hello_World")
 

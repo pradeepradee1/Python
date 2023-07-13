373520be42fa1a4a41c23ef884857ac69333b4f2
pradeepradee1 <pradeepradee1@gmail.com>
2023-07-13 08:43:14 +0530

    Added Pattern

--- tmp.py
+++ tmp.py
@@ -1,8 +1,17 @@
-import os,sys
-try:
-    print("try")
-    os._exit(0)
-except NameError:
-    print("except")
-finally:
-    print("finally")
+# import os,sys
+# try:
+#     print("try")
+#     os._exit(0)
+# except NameError:
+#     print("except")
+# finally:
+#     print("finally")
+
+
+a=[3,2,2,3]
+#a.remove(3)
+for i in range(0,a.count(3)):
+    a.remove(3)
+    a.append("_")
+print(a)
+

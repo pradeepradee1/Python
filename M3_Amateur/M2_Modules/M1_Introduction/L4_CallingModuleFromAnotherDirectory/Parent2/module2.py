2cc8b8e84d6c494b7207502b8d391bce47063b67
Pradeep <pradeepradee1@gmail.com>
2024-11-04 09:32:08 +0530

    added the module in python

---
+++ M3_Amateur/M2_Modules/M1_Introduction/L4_CallingModuleFromAnotherDirectory/Parent2/module2.py
@@ -0,0 +1,34 @@
+import sys
+import os
+
+# SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+# print(SCRIPT_DIR)
+# sys.path.append(os.path.dirname(SCRIPT_DIR))
+# print(SCRIPT_DIR)
+# # Add the module's directory to the path
+# sys.path.append(os.path.abspath('../Parent1'))
+# print(SCRIPT_DIR)
+# print(sys.path)
+# # Now you can import your module
+# # import my_module
+
+# module_dir = os.path.abspath(os.path.join('..', 'Parent1]'))
+# sys.path.append(module_dir)
+# print("Updated sys.path:", sys.path)
+
+
+module_path = os.path.abspath(os.path.join('..', 'Parent1', 'SubParnet1', 'module1.py'))
+print("Module path:", module_path)
+
+
+import importlib.util
+spec = importlib.util.spec_from_file_location("my_module", module_path)
+my_module = importlib.util.module_from_spec(spec)
+spec.loader.exec_module(my_module)
+
+# from module1 import first
+my_module.first()
+
+
+# Use your module
+
